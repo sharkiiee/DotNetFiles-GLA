237fa2548e62f2a8f9a3fd67153247a46746ad9c
sharkiiee <sarthak231203@gmail.com>
2025-07-31 11:26:16 +0530

    Add project files.

---
+++ DotNetFiles-GLA/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class FinancialCalculator
+{
+    static void Main()
+    {
+        int[] transactions = { 200, -150, 340, 500, -100 };
+        int totalSum = 0;
+
+        foreach (int amount in transactions)
+        {
+            totalSum += amount;
+        }
+
+        Console.WriteLine("Total sum of all transactions: " + totalSum);
+    }
+}
